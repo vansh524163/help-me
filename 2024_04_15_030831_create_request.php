b80b90f08e7b041d49ff485bb839dd328f015ec3
vansh524163 <157708281+vansh524163@users.noreply.github.com>
2024-04-15 10:19:59 +0530

    Create 2024_04_15_030831_create_request.php

---
+++ 2024_04_15_030831_create_request.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('request', function (Blueprint $table) {
+            $table->id();
+            $table->string('User_Name');
+            $table->string('User_Message');
+            $table->string('User_email');
+            $table->string('User_Subject');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('request');
+    }
+};
